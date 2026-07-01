d15f32c0fe09dc9cce242593313d9db839ca2536
Sean Shmilov <sean.shmelyov@gmail.com>
2026-07-01 09:04:51 +0300

    Initial Terraform setup for EKS, ECR, and ALB

---
+++ eks.tf
@@ -0,0 +1,29 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "fifaapp-cluster"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access  = true
+  
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    general = {
+      instance_types = ["t3.medium"]
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+  tags = {
+    Environment = "production"
+    Project     = "FifaApp"
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  cloud {
+    organization = "YOUR_ORG_NAME_HERE" # Replace with your Terraform Cloud Organization name
+
+    workspaces {
+      name = "fifaapp-workspace"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# These data sources allow the Kubernetes/Helm providers to authenticate with the EKS cluster
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
